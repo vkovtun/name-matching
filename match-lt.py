2fad6e4a41c31cdea976674a2ae950393ce59030
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-03-20 12:04:48 +0100

    Fixes file locations.

--- match-lt.py
+++ match-lt.py
@@ -2,7 +2,7 @@ import linktransformer as lt
 import pandas as pd
 
 # Load the data
-rivals_data = pd.read_csv('data_test/rivals_data_fuzzy.csv')
+rivals_data = pd.read_csv('data/rivals_data_fuzzy.csv')
 siren_data = pd.read_csv('data/siren_database_trimmed.csv')
 
 rivals_data['RIVAL_NAME'] = rivals_data['RIVAL_NAME'].astype(str)
@@ -11,7 +11,7 @@ siren_data['RIVAL_NAME'] = siren_data['RIVAL_NAME'].astype(str)
 rivals_data['apen2'] = rivals_data['apen2'].fillna(0)
 siren_data['apen2'] = siren_data['apen2'].fillna(0)
 
-result_csv_file = 'results_test/result-1-m.csv'
+result_csv_file = 'results/result-1-m.csv'
 merged_data = lt.merge(rivals_data, siren_data, merge_type='1:m', on=['RIVAL_NAME']) #, model='dell-research-harvard/lt-wikidata-comp-fr')
 merged_data.to_csv(result_csv_file)
 
