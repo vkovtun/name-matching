2683a1b1f9bae48950679dac574b265e72a3fb26
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-03-20 11:57:39 +0100

    Added info about the resulting file as the final message.

--- match-lt.py
+++ match-lt.py
@@ -1,10 +1,9 @@
 import linktransformer as lt
 import pandas as pd
-import os
 
 # Load the data
-rivals_data = pd.read_csv(os.path.join('data_test', 'rivals_data_fuzzy_test.csv'))
-siren_data = pd.read_csv(os.path.join('data', 'siren_database_trimmed.csv'))
+rivals_data = pd.read_csv('data_test/rivals_data_fuzzy.csv')
+siren_data = pd.read_csv('data/siren_database_trimmed.csv')
 
 rivals_data['RIVAL_NAME'] = rivals_data['RIVAL_NAME'].astype(str)
 siren_data['RIVAL_NAME'] = siren_data['RIVAL_NAME'].astype(str)
@@ -12,7 +11,8 @@ siren_data['RIVAL_NAME'] = siren_data['RIVAL_NAME'].astype(str)
 rivals_data['apen2'] = rivals_data['apen2'].fillna(0)
 siren_data['apen2'] = siren_data['apen2'].fillna(0)
 
-merged_data = lt.merge(rivals_data, siren_data, merge_type='1:1', on=['RIVAL_NAME']) #, model='dell-research-harvard/lt-wikidata-comp-fr')
-merged_data.to_csv('results_test/result-1-1-test.csv')
+result_csv_file = 'results_test/result-1-m.csv'
+merged_data = lt.merge(rivals_data, siren_data, merge_type='1:m', on=['RIVAL_NAME']) #, model='dell-research-harvard/lt-wikidata-comp-fr')
+merged_data.to_csv(result_csv_file)
 
-print("Success")
+print(f"Results put to {result_csv_file}")
